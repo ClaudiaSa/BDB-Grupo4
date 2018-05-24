e4a4fe031047fae7d2662c0697ae0ffd9346e0af
= <=>
2018-05-24 17:27:46 +0200

    Prototipo 1 Código Java

---
+++ Prototipo 1 Java/src/main/java/BDBG4/JavaImplementationDBSG/MySQL.java
@@ -0,0 +1,43 @@
+package BDBG4.JavaImplementationDBSG;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQL {
+	public static void main(String args[]) {
+		Connection c = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			c = DriverManager.getConnection("jdbc:mysql://localhost:3306/CHEBI", "root", "admin");
+
+			c.setAutoCommit(false);
+			System.out.println("Opened database successfully");
+
+			new QueryResultCompound(c, "select * from Compounds WHERE Compounds.id = ANY (\r\n"
+					+ "	select Distinct parent_id from Compounds where id =ANY(\r\n"
+					+ "											select Distinct compound_id from Structures where dimension <> '1D'\r\n"
+					+ "											)\r\n" + "	)").ControlTime();
+
+			new QueryResultCompound(c, "SELECT * FROM COMPOUNDS WHERE PARENT_ID = 27732;").ControlTime();
+
+			new QueryResultCompound(c,
+					"SELECT C.* FROM names N INNER JOIN compounds C ON N.compound_id = C.id WHERE N.name = 'Cyanure' ;")
+							.ControlTime();
+
+			new QueryResultCompound(c,
+					"SELECT C.* FROM chemical_data D INNER JOIN compounds C ON D.compound_id = C.id WHERE D.chemical_data =  'C6H7N' ;")
+							.ControlTime();
+
+			new QueryResultStructures(c,
+					"SELECT S.* FROM compounds C INNER JOIN structures S ON S.compound_id = C.id WHERE C.id = 34565 ;")
+							.ControlTime();
+									
+			c.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Operation done successfully");
+	}
+}
---
+++ Prototipo 1 Java/src/main/java/BDBG4/JavaImplementationDBSG/noIndex/QueryResultCompound.java
@@ -0,0 +1,34 @@
+package BDBG4.JavaImplementationDBSG.noIndex;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import BDBG4.JavaImplementationDBSG.QueryTemplate;
+
+public class QueryResultCompound extends QueryTemplate {
+	int id, parent_id, star;
+	String name, source, chebi_accession, status, definition, modified_on, created_by;
+
+	public QueryResultCompound(Connection con, String query) {
+		super(con, query);
+	}
+
+	public void ComputeResults(ResultSet rs) throws SQLException {
+		this.id = rs.getInt("id");
+		this.name = rs.getString("name");
+		this.source = rs.getString("source");
+		this.parent_id = rs.getInt("parent_id");
+		this.chebi_accession = rs.getString("chebi_accession");
+		this.status = rs.getString("status");
+		this.definition = rs.getString("definition");
+		this.star = rs.getInt("star");
+		this.modified_on = rs.getString("modified_on");
+		this.created_by = rs.getString("created_by");
+
+		System.out.println("Id: " + id + "\n" + "Name:" + name + "\n" + "Source: " + source + "\n" + "Parent id: "
+				+ parent_id + "\n" + "Chebi Accession: " + chebi_accession + "\n" + "Status: " + status + "\n"
+				+ "Definition: " + definition + "\n" + "Star: " + star + "\n" + "Modified on: " + modified_on + "\n"
+				+ "Created by: " + created_by + "\n");
+	}
+}
